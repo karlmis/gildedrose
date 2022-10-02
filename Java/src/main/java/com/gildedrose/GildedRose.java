197a083c3ce4344fc0966045c06bd5ecc6a40573
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 21:32:08 +0200

    use Item as parameter everywhere

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,77 +8,77 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            updateQuality(i);
+        for (Item item : items) {
+            updateQuality(item);
         }
     }
 
-    private void updateQuality(int i) {
-        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+    private void updateQuality(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             return;
         }
-        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            updateQualityBackstagePass(i);
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            updateQualityBackstagePass(item);
             return;
         }
-        if (items[i].name.equals("Aged Brie")){
-            updateQualityAgedBrie(i);
+        if (item.name.equals("Aged Brie")){
+            updateQualityAgedBrie(item);
             return;
         }
-        updateQualityDefault(i);
+        updateQualityDefault(item);
     }
 
-    private void updateQualityDefault(int i) {
-        items[i].sellIn = items[i].sellIn - 1;
+    private void updateQualityDefault(Item item) {
+        item.sellIn = item.sellIn - 1;
 
-        if (items[i].quality > 0) {
-            items[i].quality = items[i].quality - 1;
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
 
-        if (items[i].sellIn < 0) {
-            if (items[i].quality > 0) {
-                items[i].quality = items[i].quality - 1;
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
 
-    private void updateQualityAgedBrie(int i) {
-        items[i].sellIn = items[i].sellIn - 1;
+    private void updateQualityAgedBrie(Item item) {
+        item.sellIn = item.sellIn - 1;
 
-        if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
 
         }
 
-        if (items[i].sellIn < 0) {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
 
-    private void updateQualityBackstagePass(int i) {
-        items[i].sellIn = items[i].sellIn - 1;
+    private void updateQualityBackstagePass(Item item) {
+        item.sellIn = item.sellIn - 1;
 
-        if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
 
-            if (items[i].sellIn < 10) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            if (item.sellIn < 10) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
 
-            if (items[i].sellIn < 5) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            if (item.sellIn < 5) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
 
 
-        if (items[i].sellIn < 0) {
-            items[i].quality = 0;
+        if (item.sellIn < 0) {
+            item.quality = 0;
         }
     }
 
