ed5b8ce882b47325f91558bc6dd479dc83af446e
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 21:37:27 +0200

    introduce ItemWrapper & ItemWrapperFactory & put the logic in there

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,72 +14,8 @@ class GildedRose {
     }
 
     private void updateQuality(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            updateQualityBackstagePass(item);
-            return;
-        }
-        if (item.name.equals("Aged Brie")){
-            updateQualityAgedBrie(item);
-            return;
-        }
-        updateQualityDefault(item);
+        new ItemWrapperFactory().createWrapper(item).update();
     }
 
-    private void updateQualityDefault(Item item) {
-        item.sellIn = item.sellIn - 1;
-
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        }
-    }
-
-    private void updateQualityAgedBrie(Item item) {
-        item.sellIn = item.sellIn - 1;
-
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-        }
-
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
-        }
-    }
-
-    private void updateQualityBackstagePass(Item item) {
-        item.sellIn = item.sellIn - 1;
-
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.sellIn < 10) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
-            if (item.sellIn < 5) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
-
-
-        if (item.sellIn < 0) {
-            item.quality = 0;
-        }
-    }
 
 }
---
+++ Java/src/main/java/com/gildedrose/ItemWrapperFactory.java
@@ -0,0 +1,98 @@
+package com.gildedrose;
+
+public class ItemWrapperFactory {
+
+    public ItemWrapper createWrapper(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return new ItemWrapper() {
+                @Override
+                public void update() {
+
+                }
+            };
+        }
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return new ItemWrapper() {
+                @Override
+                public void update() {
+                    updateQualityBackstagePass(item);
+                }
+
+                private void updateQualityBackstagePass(Item item) {
+                    item.sellIn = item.sellIn - 1;
+
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+
+                        if (item.sellIn < 10) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn < 5) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+
+
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
+                    }
+                }
+            };
+        }
+        if (item.name.equals("Aged Brie")) {
+            return new ItemWrapper() {
+                @Override
+                public void update() {
+                    updateQualityAgedBrie(item);
+                }
+
+                private void updateQualityAgedBrie(Item item) {
+                    item.sellIn = item.sellIn - 1;
+
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+
+                    }
+
+                    if (item.sellIn < 0) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+
+            };
+
+        }
+        return new ItemWrapper() {
+            @Override
+            public void update() {
+                updateQualityDefault(item);
+            }
+
+            private void updateQualityDefault(Item item) {
+                item.sellIn = item.sellIn - 1;
+
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            }
+
+
+        };
+
+
+    }
+}
+
