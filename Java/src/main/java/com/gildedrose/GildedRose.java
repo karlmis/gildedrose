ad1407f4f93a491bc7fd8de15315ba4ffc0b377d
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 20:52:59 +0200

    for sulfuras replace checks for name with correct value(equals("sulfuras") and simplify

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -69,37 +69,5 @@ class GildedRose {
     }
 
     private void updateQualitySulfuras(int i) {
-        if (items[i].name.equals("Aged Brie")
-            || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (items[i].sellIn < 0) {
-            if (!items[i].name.equals("Aged Brie")) {
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    items[i].quality = 0;
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-            }
-        }
     }
 }
