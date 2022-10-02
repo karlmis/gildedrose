c3c02b16744f2d30971e80219dd418ad5de8ad4b
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 23:42:46 +0200

    clean up GildedRose class

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,23 +1,29 @@
 package com.gildedrose;
 
+import com.gildedrose.itemwrapper.ItemWrapper;
 import com.gildedrose.itemwrapper.ItemWrapperFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 class GildedRose {
     Item[] items;
+    private final List<ItemWrapper> itemWrappers;
 
     public GildedRose(Item[] items) {
         this.items = items;
+        ItemWrapperFactory itemWrapperFactory = new ItemWrapperFactory();
+        itemWrappers = Arrays.stream(items)
+            .map(itemWrapperFactory::createWrapper)
+            .collect(toList());
     }
 
     public void updateQuality() {
-        for (Item item : items) {
-            updateQuality(item);
-        }
+        itemWrappers.forEach(ItemWrapper::update);
     }
 
-    private void updateQuality(Item item) {
-        new ItemWrapperFactory().createWrapper(item).update();
-    }
 
 
 }
