0f6cd9b6ae3b36897219ef311448f7d740da5cf5
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 21:04:13 +0200

    simple math

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -24,13 +24,13 @@ class GildedRose {
                 items[i].quality = items[i].quality + 1;
 
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11 - 1) {
+                    if (items[i].sellIn < 10) {
                         if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
                     }
 
-                    if (items[i].sellIn < 6 - 1) {
+                    if (items[i].sellIn < 5) {
                         if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
