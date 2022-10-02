ea948ee7e875eb44921c2e7270890a98c8814c56
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 20:59:53 +0200

    quality is changes depending on sellin, then sellin is changed, then quality again: so we change sellin first

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,19 +16,21 @@ class GildedRose {
     }
 
     private void updateQualityNonSulfuras(int i) {
+        int oldSellin = items[i].sellIn;
+        items[i].sellIn = items[i].sellIn - 1;
         if (items[i].name.equals("Aged Brie")
             || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (items[i].quality < 50) {
                 items[i].quality = items[i].quality + 1;
 
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11) {
+                    if (oldSellin < 11) {
                         if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
                     }
 
-                    if (items[i].sellIn < 6) {
+                    if (oldSellin < 6) {
                         if (items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
@@ -41,7 +43,7 @@ class GildedRose {
             }
         }
 
-        items[i].sellIn = items[i].sellIn - 1;
+
 
         if (items[i].sellIn < 0) {
             if (!items[i].name.equals("Aged Brie")) {
