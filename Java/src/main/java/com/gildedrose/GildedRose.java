bf1c41dc70808ce10207f06c9e0e5b316c11d204
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 21:14:39 +0200

    use the fixed value for items[i].name(equals("Backstage passes to a TAFKAL80ETC concert")) in updateQualityDefault & updateQualityBackstagePass

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -27,24 +27,10 @@ class GildedRose {
     private void updateQualityDefault(int i) {
         items[i].sellIn = items[i].sellIn - 1;
 
-        if (items[i].name.equals("Aged Brie")
-            || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (items[i].name.equals("Aged Brie")) {
             if (items[i].quality < 50) {
                 items[i].quality = items[i].quality + 1;
 
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 10) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 5) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
             }
         } else {
             if (items[i].quality > 0) {
@@ -52,16 +38,10 @@ class GildedRose {
             }
         }
 
-
-
         if (items[i].sellIn < 0) {
             if (!items[i].name.equals("Aged Brie")) {
-                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                } else {
-                    items[i].quality = 0;
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -74,47 +54,25 @@ class GildedRose {
     private void updateQualityBackstagePass(int i) {
         items[i].sellIn = items[i].sellIn - 1;
 
-        if (items[i].name.equals("Aged Brie")
-            || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
 
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 10) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 5) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        } else {
-            if (items[i].quality > 0) {
-                items[i].quality = items[i].quality - 1;
-            }
-        }
-
-
-
-        if (items[i].sellIn < 0) {
-            if (!items[i].name.equals("Aged Brie")) {
-                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                } else {
-                    items[i].quality = 0;
-                }
-            } else {
+            if (items[i].sellIn < 10) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
                 }
             }
+
+            if (items[i].sellIn < 5) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+            }
+        }
+
+
+        if (items[i].sellIn < 0) {
+            items[i].quality = 0;
         }
     }
 
