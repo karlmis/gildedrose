4c55913176e98cfeceeb95f3f714a9297595d6cd
saaaa12531 <karl.missiaen@mow.vlaanderen.Be>
2022-10-02 21:16:12 +0200

    duplicated the updateQualityDefault again for aged brie and put an if/else before the differentiate the call

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,10 +21,41 @@ class GildedRose {
             updateQualityBackstagePass(i);
             return;
         }
+        if (items[i].name.equals("Aged Brie")){
+            updateQualityAgedBrie(i);
+            return;
+        }
         updateQualityDefault(i);
     }
 
     private void updateQualityDefault(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+
+        if (items[i].name.equals("Aged Brie")) {
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
+
+            }
+        } else {
+            if (items[i].quality > 0) {
+                items[i].quality = items[i].quality - 1;
+            }
+        }
+
+        if (items[i].sellIn < 0) {
+            if (!items[i].name.equals("Aged Brie")) {
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+            }
+        }
+    }
+
+    private void updateQualityAgedBrie(int i) {
         items[i].sellIn = items[i].sellIn - 1;
 
         if (items[i].name.equals("Aged Brie")) {
